d963391a5f9a85ff0c2c15819342baf5f2b7bfad
giovany <230314@utxicotepec.edu.mx>
2024-09-17 15:23:52 -0600

    Practica04
    ejercicio3 -tipos de datos,string,big int y symbol

---
+++ JS/Ejercicio03.js
@@ -0,0 +1,35 @@
+// strings o cadenas de texto
+//const alumno="juan"
+//let producto='monitor 20 pulgados'
+
+//const numero="30"
+//const numero2= 30
+
+
+ //console.log(typeof numero)
+ //console.log (typeof numero2)
+
+ //const numeroGrande= BigInt(1000000000)
+ //console.log(typeof numeroGrande )
+ //const numero = 10
+ //const numero2= 20
+ //console.log (numero + Number(numeroGrande))
+
+
+
+//const numero = "30"
+//const numero2 = 30
+//console.log(typeofstring(numero2))
+//console.log(typeof Numbernumero)
+
+
+//const primerSymbol = Symbol(30)
+//const segundoSymbol = Symbol(30)
+//console.log(primerSymbol===segundoSymbol)
+//console.log(primerSymbol.valueOf())
+
+
+//null
+const descuento=null
+ console.log(typeof descuento)
+
