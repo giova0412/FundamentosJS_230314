cf801eda86432ab19847ed8405475e34b19dd2a2
unknown <230314@utxicotepec.edu.mx>
2024-09-19 17:12:08 -0600

    practica5
    objetos

--- JS/Ejercicio03.js
+++ JS/Ejercicio03.js
@@ -1,32 +1,37 @@
 
 
 /// objetos
-const nombre="tablet"
-const precio =300
-const disponible=true
+
 
 const producto1 = {
-    nombre: "tablet",
-    precio:300,
-    disponible:true
+    nombre: "tablet 9\"",
+    marca:"mac",
+    modelo:"ipad",
+    costo_compra:11500.25,
+    costo_venta:15400,
+    disponible:true,
+    SKU:Symbol("12345"),
+    colores: ["blanco","negro","rosa","azul","amarillo"]
+
+
+
 }
- console.log(producto)
- console.log(producto)
+ console.log(producto1)
+ console.table(producto1)
 
- console.log(producto.nombre)
- console.log(producto.precio)
- console.log(producto.disponible)
+//acceder a las propiedades de un objeto
+console.warn("leyendo las propiedades de un objeto y sus tipos de datos ")
+console.log(`nombre del producto ${producto1.nombre}, que es del tipo:${typeof}`)
 
 
- const { nombre2 , precio2 , disponible2 }=producto
- console.log(nombre2)
- console.log(precio2)
- console.log(disponible)
+ //const { nombre2 , precio2 , disponible2 }=producto1
+// console.log(nombre2)
+ //console.log(precio2)
+ //console.log(disponible)
+
 //objeto literal 
+
 const autenticado=true 
 const usuario ="giovany"
-const nuevoObjeto={
-    autenticado: autenticado,
-    usuario: usuario
-}
-console.table(nuevoObjeto)
+//}
+//console.table(nuevoObjeto)
